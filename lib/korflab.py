3276e07e2e2f498467c23faeb2499e855b1e9eb8
iankorf <ifkorf@gmail.com>
2025-05-08 20:41:59 -0700

    up

--- lib/korflab.py
+++ lib/korflab.py
@@ -106,13 +106,13 @@ def translate(seq, frame=0):
 ## File Readers ##
 
 def getfp(filename):
-    """Returns a file pointer for reading based on file name"""
-    if   filename.endswith('.gz'):
-        return gzip.open(filename, 'rt', encoding='ISO-8859-1')
-    elif filename == '-':
-        return sys.stdin
-    else:
-        return open(filename)
+	"""Returns a file pointer for reading based on file name"""
+	if   filename.endswith('.gz'):
+		return gzip.open(filename, 'rt', encoding='ISO-8859-1')
+	elif filename == '-':
+		return sys.stdin
+	else:
+		return open(filename)
 
 def readfasta(filename):
 	"""Simple fasta file iterator: yields defline, seq"""
